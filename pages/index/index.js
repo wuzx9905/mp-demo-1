26f07fe41ad32cd95a31d9688ecabc62d1c1fc77
wuzx9905 <853529718@qq.com>
2022-02-28 18:27:12 +0800

    develop
    完成基本功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,45 @@
 // index.js
 // 获取应用实例
-const app = getApp()
 
 Page({
   data: {
-    motto: '你好',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    message: ['猜大了','猜小了','猜对了！'],
+    result: '',
+    target: 0,
+    x: 0,
+    y: 0,
+    index: 0,
+    arrayX: [0,1,2,3,4,5,6,7,8,9],
+    arrayY: [0,1,2,3,4,5,6,7,8,9],
+    targetNumber: '?'
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  x:function(){
+    this.setData({n:this.data.n +1})
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
+  setX:function(e){
+    this.setData({x: e.detail.value})
+  },
+  setY:function(e){
+    this.setData({y: e.detail.value})
+  },
+  begin:function(){
+    this.setData({target: Math.round(Math.random()*100)})
+    console.log(this.data.target)
+  },
+  return:function(){
+    this.setData({x: 0});
+    this.setData({y: 0});
+  },
+  guess:function(){
+    const number = parseInt(this.data.x + this.data.y);
+    if(number === this.data.target){
+      this.setData({result: this.data.message[2]});
+      this.setData({targetNumber: (this.data.x + this.data.y).toString()});
+    }else if(number > this.data.target){
+      this.setData({result: this.data.message[0]})
+    }else{
+      this.setData({result: this.data.message[1]})
     }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    this.return();
   }
 })
